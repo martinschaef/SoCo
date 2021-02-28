77cf73caed90c54908cdc877db026277ca55e910
Martin Schaef <schaef@amazon.com>
2021-02-28 17:21:17 -0500

    make it look nice and search for player

--- examples/webapp/index.py
+++ examples/webapp/index.py
@@ -8,15 +8,16 @@ import simplejson as json
 
 from flask import Flask, render_template, url_for
 
-from soco import SoCo
+from soco import SoCo, discover
 
 import settings
+import find_player
 
 app = Flask(__name__)
 
 app.config.from_pyfile('settings.py')
 
-sonos = SoCo(app.config['SPEAKER_IP'])
+sonos = find_player.find_speaker_with_music()
 
 def gen_sig():
     hash_string = "{}{}{}".format(app.config['ROVI_API_KEY'], app.config['ROVI_SHARED_SECRET'], repr(int(time.time()))).encode('utf-8')
@@ -38,46 +39,60 @@ def get_track_image(artist, album, image_uri):
 
 @app.route("/play")
 def play():
-    sonos.play()
+    if sonos:
+        sonos.play()
 
     return 'Ok'
 
 @app.route("/pause")
 def pause():
-    sonos.pause()
+    if sonos:
+        sonos.pause()
 
     return 'Ok'
 
 @app.route("/next")
 def next():
-    sonos.next()
+    if sonos:
+        sonos.next()
 
     return 'Ok'
 
 @app.route("/previous")
 def previous():
-    sonos.previous()
+    if sonos:
+        sonos.previous()
 
     return 'Ok'
 
+def get_track():
+    global sonos
+    # Check if the current player is still playing music. If not, find another one.
+    sonos = find_player.find_speaker_with_music(sonos)    
+    track = {'title': '', 'artist': '', 'album': '', 'album_art': '',
+            'position': '', 'speaker': ''}
+    if sonos:
+        track = sonos.get_current_track_info()
+        if sonos.player_name:
+            track['speaker'] = "Playing on " + sonos.player_name
+    return track
+    
+
 @app.route("/info-light")
 def info_light():
-    track = sonos.get_current_track_info()
-
+    track = get_track()
     return json.dumps(track)
 
 @app.route("/info")
 def info():
-    track = sonos.get_current_track_info()
-
+    track = get_track()
     track['image'] = get_track_image(track['artist'], track['album'], track['album_art'])
 
     return json.dumps(track)
 
 @app.route("/")
 def index():
-    track = sonos.get_current_track_info()
-
+    track = get_track()
     track['image'] = get_track_image(track['artist'], track['album'], track['album_art'])
 
     return render_template('index.html', track=track)
